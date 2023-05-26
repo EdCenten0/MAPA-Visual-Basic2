bdedf5453085c9b5e4515f90ceb6e0a554c40b7d
Ed Centeno <cchavarriacenteno8@gmail.com>
2023-05-26 13:03:40 -0600

    Update Frm_Materiales.vb

--- MAPA Visual Basic 2/MAPA Visual Basic 2/Vistas/Frm_Materiales.vb
+++ MAPA Visual Basic 2/MAPA Visual Basic 2/Vistas/Frm_Materiales.vb
@@ -2,6 +2,11 @@
     Private Sub Frm_Materiales_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'TODO: esta línea de código carga datos en la tabla 'MAPADataSet.materiales' Puede moverla o quitarla según sea necesario.
         Me.MaterialesTableAdapter.Fill(Me.MAPADataSet.materiales)
+
+
+
+
+
         cb_unidad_medida.DropDownStyle = ComboBoxStyle.DropDownList
     End Sub
 
