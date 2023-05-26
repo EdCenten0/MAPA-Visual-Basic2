c3821bc7c0c6ee955e73f5b998b4f6c13b05faa5
isabel <isabel.aguilar12833@est.uca.edu>
2023-05-26 13:54:50 -0600

    Cambios Forms

--- MAPA Visual Basic 2/MAPA Visual Basic 2/Vistas/Frm_Taller.vb
+++ MAPA Visual Basic 2/MAPA Visual Basic 2/Vistas/Frm_Taller.vb
@@ -1,3 +1,21 @@
 ﻿Public Class Frm_Taller
 
+    Private Sub Frm_Taller_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: esta línea de código carga datos en la tabla 'MAPADataSet.taller' Puede moverla o quitarla según sea necesario.
+        Me.TallerTableAdapter.Fill(Me.MAPADataSet.taller)
+
+    End Sub
+
+    Private Sub btn_guardar_Click(sender As Object, e As EventArgs) Handles btn_guardar.Click
+
+        MessageBox.Show("Se ha guardado el registro: " & txtNombre.Text)
+
+        Me.TallerTableAdapter.Guardar(txtNombre.Text, txtDireccion.Text, txtTelefono.Text, txtEmail.Text, 1)
+        Me.TallerTableAdapter.Fill(Me.MAPADataSet.taller)
+
+    End Sub
+
+
+
+
 End Class
