f37ea4544f60cbdd0e40eb3b1ae406d6718b5c19
Ed Centeno <cchavarriacenteno8@gmail.com>
2023-05-30 02:58:12 -0600

    Update Frm_Materiales.vb

--- MAPA Visual Basic 2/MAPA Visual Basic 2/Vistas/Frm_Materiales.vb
+++ MAPA Visual Basic 2/MAPA Visual Basic 2/Vistas/Frm_Materiales.vb
@@ -64,7 +64,7 @@ Public Class Frm_Materiales
     End Sub
 
     Private Sub comboBox()
-        Dim connection As New SqlConnection("Data Source=DENISSE;Initial Catalog=MAPA;Persist Security Info=True;User ID=sa;Password=1234")
+        Dim connection As New SqlConnection("Data Source=CARLOSCent;Initial Catalog=MAPA;Persist Security Info=True;User ID=sa;Password=1234")
         Dim command As New SqlCommand("SELECT * FROM pedidos", connection)
         Dim adapter As New SqlDataAdapter(command)
         Dim table As New DataTable
