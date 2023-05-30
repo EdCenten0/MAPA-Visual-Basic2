46cf7fbe8aa27f743c142cfbc8a420a134e87401
FranciscoMelen10 <francisco.melendez14454@est.uca.edu.ni>
2023-05-30 13:56:41 -0600

    Agrege las validaciones en los formularios de Frm_Usuarios y Frm_Agregar_Usuarios

--- MAPA Visual Basic 2/MAPA Visual Basic 2/Vistas/Frm_Agregar_Usuario.vb
+++ MAPA Visual Basic 2/MAPA Visual Basic 2/Vistas/Frm_Agregar_Usuario.vb
@@ -9,13 +9,46 @@
     End Sub
 
     Private Sub bt_registrar_Click(sender As Object, e As EventArgs) Handles bt_registrar.Click
-        MessageBox.Show("Se ha guardado el usuario: " & txt_usuario.Text)
-        Me.UsuariosTableAdapter.Guardar(txt_usuario.Text, txt_clave.Text, txt_correo.Text, cb_rol.SelectedValue)
-        Me.UsuariosTableAdapter.Fill(Me.MAPADataSet.usuarios)
-        Me.Close()
+
+
+        If Not Me.txt_usuario.Text = "" And Not Me.txt_correo.Text = "" And Not Me.txt_clave.Text = "" Then
+
+
+            If Me.txt_clave.TextLength >= 8 Then
+
+                If Me.txt_usuario.TextLength <= 50 Then
+
+                    If Me.txt_clave.TextLength <= 50 Then
+
+                        If Me.txt_correo.TextLength <= 50 Then
+                            MessageBox.Show("Se ha guardado el usuario: " & txt_usuario.Text)
+                            Me.UsuariosTableAdapter.Guardar(txt_usuario.Text, txt_clave.Text, txt_correo.Text, cb_rol.SelectedValue)
+                            Me.UsuariosTableAdapter.Fill(Me.MAPADataSet.usuarios)
+                            Me.Close()
+
+                        Else
+                            MessageBox.Show("Error de correo electronico, debe tener menos de 50 caracteres")
+                        End If
+
+                    Else
+                        MessageBox.Show("Error de contraseña, debe tener menos de 50 caracteres")
+                    End If
+
+                Else
+                    MessageBox.Show("Error de nombre de usuario, debe tener menos de 50 caracteres")
+                End If
+
+            Else
+                MessageBox.Show("La contraseña debe tener más de 8 caracteres")
+            End If
+
+        Else
+        MessageBox.Show("Llene los campos vacios")
+        End If
     End Sub
 
     Private Sub check_mostrar_clave_CheckedChanged(sender As Object, e As EventArgs) Handles check_mostrar_clave.CheckedChanged
+
         If check_mostrar_clave.Checked = False Then
 
             txt_clave.PasswordChar = "*"
