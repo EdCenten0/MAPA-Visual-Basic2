eace1187896ce54335424ffcf23beda693504801
Ed Centeno <cchavarriacenteno8@gmail.com>
2023-05-30 00:17:48 -0600

    Vaciar campos

--- MAPA Visual Basic 2/MAPA Visual Basic 2/Vistas/Frm_Materiales_Por_Pedido.vb
+++ MAPA Visual Basic 2/MAPA Visual Basic 2/Vistas/Frm_Materiales_Por_Pedido.vb
@@ -13,6 +13,7 @@ Public Class Frm_Materiales_Por_Pedido
 
         Me.Materiales_por_pedidoTableAdapter.InsertQuery(txt_material.Text, txt_descripcion.Text, txt_cantidad.Text, cb_unidad_medida.Text, txt_precio_unidad.Text, txt_precio_total.Text, id_pedido)
         Me.Materiales_por_pedidoTableAdapter.Fill(Me.MAPADataSet.materiales_por_pedido, id_pedido)
+        vaciar_campos()
     End Sub
 
     Private Sub btn_editar_Click(sender As Object, e As EventArgs) Handles btn_editar.Click
@@ -20,11 +21,26 @@ Public Class Frm_Materiales_Por_Pedido
 
         Me.Materiales_por_pedidoTableAdapter.Update1(txt_material.Text, txt_descripcion.Text, txt_cantidad.Text, cb_unidad_medida.Text, txt_precio_unidad.Text, txt_precio_total.Text, id_pedido, lbl_id_material.Text)
         Me.Materiales_por_pedidoTableAdapter.Fill(Me.MAPADataSet.materiales_por_pedido, id_pedido)
+        vaciar_campos()
     End Sub
 
     Private Sub btn_eliminar_Click(sender As Object, e As EventArgs) Handles btn_eliminar.Click
         MessageBox.Show("Se ha editado el registro:")
         Me.Materiales_por_pedidoTableAdapter.Delete1(lbl_id_material.Text)
         Me.Materiales_por_pedidoTableAdapter.Fill(Me.MAPADataSet.materiales_por_pedido, id_pedido)
+        vaciar_campos()
+    End Sub
+
+    Private Sub btn_vaciar_campos_Click(sender As Object, e As EventArgs) Handles btn_vaciar_campos.Click
+        vaciar_campos()
+    End Sub
+
+    Private Sub vaciar_campos()
+        txt_material.Clear()
+        txt_descripcion.Clear()
+        txt_precio_unidad.Clear()
+        txt_cantidad.Clear()
+        txt_precio_total.Clear()
+        cb_unidad_medida.SelectedIndex = 0
     End Sub
 End Class
