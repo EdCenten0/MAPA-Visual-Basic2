d7d1bf81873feee337eba26e067950c9c74bc0b9
Ed Centeno <cchavarriacenteno8@gmail.com>
2023-06-03 01:31:10 -0600

    Cambios pequeños de configuración

--- MAPA Visual Basic 2/MAPA Visual Basic 2/My Project/Settings.Designer.vb
+++ MAPA Visual Basic 2/MAPA Visual Basic 2/My Project/Settings.Designer.vb
@@ -15,7 +15,7 @@ Option Explicit On
 Namespace My
     
     <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
-     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0"),  _
+     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.7.0.0"),  _
      Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
     Partial Friend NotInheritable Class MySettings
         Inherits Global.System.Configuration.ApplicationSettingsBase
@@ -57,8 +57,8 @@ Namespace My
         <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
          Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.ConnectionString),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-UK33DLB\SA;Initial Catalog=MAPA;Persist Security Info=True;Us"& _ 
-            "er ID=sa;Password=1234")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("Data Source=CARLOSCent;Initial Catalog=MAPA;Persist Security Info=True;User ID=sa"& _ 
+            ";Password=1234")>  _
         Public ReadOnly Property MAPAConnectionString() As String
             Get
                 Return CType(Me("MAPAConnectionString"),String)
